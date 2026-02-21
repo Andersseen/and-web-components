ec12395252018bdcddc509c13b618f974dd7f357
andersseen <andriipap01@gmail.com>
2026-02-21 16:52:07 +0100

    feat: update routing for navbar and sidebar components; adjust motion routes

--- apps/angular-workspace/projects/demo-app/src/app/motion.routes.ts
+++ apps/angular-workspace/projects/demo-app/src/app/motion.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 const motionRoutes: Routes = [
   {
     path: '',
+    redirectTo: 'attribute',
+    pathMatch: 'full',
+  },
+  {
+    path: 'attribute',
     loadComponent: () =>
       import('./pages/motion-demo/pages/attribute-demo.component'),
   },
